2d6b2efbe52d29cbdcb5654e60ca47972d5d1fcc
Nowakooo26x <nowakooo26x@gmail.com>
2020-03-23 16:22:35 +0100

    v4

--- app/src/layouts/Header.js
+++ app/src/layouts/Header.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import '../styles/Header.css';
+
+import img1 from '../img/1.jpg';
+import img2 from '../img/2.jpg';
+import img3 from '../img/3.jpg';
+
 
 const Header = () => {
     return(
-        <div>hhhh</div>
+        <>
+        <Switch>
+            <Route path="/" exact render={()=>(
+                <img src={img1}/>
+            )}/>
+
+            <Route path="/products" render={()=>(
+                <img src={img2}/>
+            )}/>
+
+            <Route path="/contact" render={()=>(
+                <img src={img3}/>
+            )}/>
+
+            <Route path="/admin" render={()=>(
+                <img src={img1}/>
+            )}/>
+
+            <Route render={()=>(
+                <img src={img1}/>
+            )}/>
+        </Switch>
+        </>
     );
 
 }
