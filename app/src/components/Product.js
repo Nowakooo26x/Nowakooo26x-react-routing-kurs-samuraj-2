a67e6e9ca43e6ed1aeedbc22e04a9322ac828d00
Nowakooo26x <nowakooo26x@gmail.com>
2020-03-26 17:37:29 +0100

    v10

---
+++ app/src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+    //match
+    console.log(props)
+    return(
+        <div>Product</div>
+    );
+
+}
+
+export default Product;
--- app/src/pages/ProductsPage.js
+++ app/src/pages/ProductsPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Product from '../components/Product';
+import { Link } from 'react-router-dom';
 
 const ProductsPage = () => {
     return(
+        <>
         <div>ProductsPage</div>
+        <Product/>
+        <Link to="/products">Powrót do listy produktów</Link>
+        </>
     );
 
 }
