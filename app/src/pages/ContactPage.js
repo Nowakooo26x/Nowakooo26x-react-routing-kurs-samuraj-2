485ba9d05389264e6509b3f870a3fbff414341ac
Nowakooo26x <nowakooo26x@gmail.com>
2020-03-26 15:42:29 +0100

    v9

--- app/src/pages/ContactPage.js
+++ app/src/pages/ContactPage.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import '../styles/ContactPage.css';
+import {Prompt} from 'react-router-dom';
 
-const ContactPage = () => {
-    return(
-        <div>ContactPage</div>
-    );
+class ContactPage extends React.Component{
+    state = {
+        value: ""
 
+    }
+    handleSubmit = (e) => {
+        e.prevventDefault()
+        this.setState({value: ""})
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+    
+    render(){
+        return(
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas...</h3>
+                    <textarea value={this.state.value} onChange={this.handleChange}></textarea>
+                    <button>Wyślij!</button>
+                </form>
+                <Prompt 
+                    when={this.state.value} 
+                    message="Masz niewypełniony formularz. Czy na pewno chcesz wyjść ?"
+                />
+            </div>
+        );
+    }
 }
 
 export default ContactPage;
