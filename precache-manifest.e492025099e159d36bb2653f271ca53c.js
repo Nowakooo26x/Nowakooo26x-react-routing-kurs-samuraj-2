408e0218d1f4b362e2007eaff2040625c278a1dc
Nowakooo26x <nowakooo26x@gmail.com>
2020-03-31 18:11:53 +0200

    Updates

---
+++ precache-manifest.e492025099e159d36bb2653f271ca53c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836152cc1ca646536003c53f6bff3181",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/index.html"
+  },
+  {
+    "revision": "f4457173b6e6f99fb198",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/css/main.39b29e6b.chunk.css"
+  },
+  {
+    "revision": "2e41684aa06c43db76db",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/js/2.4ec840a5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/js/2.4ec840a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4457173b6e6f99fb198",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/js/main.36dfc9d9.chunk.js"
+  },
+  {
+    "revision": "62bdcfa7a1b5aa6c9715",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/js/runtime-main.7b4db242.js"
+  },
+  {
+    "revision": "27492ac54c9f3f9ba695cc1dfa99755f",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/media/1.27492ac5.jpg"
+  },
+  {
+    "revision": "8f5e69909e48a6b1950ffc7cd7e00239",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/media/2.8f5e6990.jpg"
+  },
+  {
+    "revision": "68a77fcb9e07d82d15faa2190dc65a60",
+    "url": "/Nowakooo26x-react-routing-kurs-samuraj-2/static/media/3.68a77fcb.jpg"
+  }
+]);
